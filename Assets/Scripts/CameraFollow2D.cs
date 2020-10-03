7f67feaedf42e08161c287d16a012aa4b376fd68
rudolrandie <rhkim1292@gmail.com>
2020-10-03 14:38:07 -0700

    Implemented camera boundary functionality.

--- Assets/Scripts/CameraFollow2D.cs
+++ Assets/Scripts/CameraFollow2D.cs
@@ -13,6 +13,18 @@ public class CameraFollow2D : MonoBehaviour
     [SerializeField]
     Vector2 posOffset;
 
+    [SerializeField]
+    float leftLimit;
+
+    [SerializeField]
+    float rightLimit;
+
+    [SerializeField]
+    float bottomLimit;
+
+    [SerializeField]
+    float topLimit;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,17 +32,34 @@ public class CameraFollow2D : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
         // Camera's current position
         Vector3 startPos = transform.position;
 
         // Player's current position
         Vector3 endPos = player.transform.position;
+
         endPos.x += posOffset.x;
         endPos.y += posOffset.y;
         endPos.z = -10;
 
         transform.position = Vector3.Lerp(startPos, endPos, timeOffset * Time.deltaTime);
+        transform.position = new Vector3
+        (
+            Mathf.Clamp(transform.position.x, leftLimit, rightLimit),
+            Mathf.Clamp(transform.position.y, bottomLimit, topLimit),
+            transform.position.z
+        );
+    }
+
+    private void OnDrawGizmos()
+    {
+        // Draw a box around our camera boundary
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(new Vector2(leftLimit, topLimit), new Vector2(rightLimit, topLimit));
+        Gizmos.DrawLine(new Vector2(rightLimit, topLimit), new Vector2(rightLimit, bottomLimit));
+        Gizmos.DrawLine(new Vector2(rightLimit, bottomLimit), new Vector2(leftLimit, bottomLimit));
+        Gizmos.DrawLine(new Vector2(leftLimit, bottomLimit), new Vector2(leftLimit, topLimit));
     }
 }

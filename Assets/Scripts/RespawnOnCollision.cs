184324cb71d341f23a227b839b9c857231c1655d
megasha <jimnguyen123@gmail.com>
2020-10-04 01:10:13 -0700

    Added main level

---
+++ Assets/Scripts/RespawnOnCollision.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnOnCollision : MonoBehaviour
+{
+    [SerializeField] Transform RespawnLocation;
+    [SerializeField] Camera cam;
+    [SerializeField] string PlayerLayerString = "Player"; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.transform.gameObject.layer == LayerMask.NameToLayer(PlayerLayerString))
+        {
+            col.transform.position = RespawnLocation.position;
+            cam.transform.position = RespawnLocation.position;
+        }
+    }
+}

87aa0854bd479ef6aa6032c6b208854206d1fa45
megasha <jimnguyen123@gmail.com>
2020-10-05 15:06:08 -0700

    Added losing condition stuff

---
+++ Assets/Scripts/FinishFlag.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class FinishFlag : MonoBehaviour
+{
+
+    [SerializeField] GameObject scoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            DontDestroyOnLoad(scoreText);
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+}
---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameOver : MonoBehaviour
+{
+    [SerializeField] Button RestartButton;
+    [SerializeField] Button MainMenuButton;
+    [SerializeField] Button QuitButton;
+    [SerializeField] Text scoreField;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RestartButton.GetComponent<Button>().onClick.AddListener(restartGame);
+        MainMenuButton.GetComponent<Button>().onClick.AddListener(goMainMenu);
+        QuitButton.GetComponent<Button>().onClick.AddListener(goQuit);
+
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
+        scoreField.text = GameObject.Find("TextScore").GetComponent<Text>().text;
+        GameObject.Find("Canvas_n").SetActive(false);
+    }
+
+    public void DeleteAll()
+    {
+        foreach (GameObject o in Object.FindObjectsOfType<GameObject>())
+        {
+            Destroy(o);
+        }
+    }
+
+    // Update is called once per frame
+    void restartGame()
+    {
+        DeleteAll();
+        SceneManager.LoadScene("Level_1");
+    }
+
+    void goMainMenu()
+    {
+        DeleteAll();
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    void goQuit()
+    {
+        Application.Quit();
+    }
+}
